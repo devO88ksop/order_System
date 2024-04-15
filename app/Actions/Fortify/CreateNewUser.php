5fabb88529a027c8971be7d6bc0246e0f005e0f2
kyawswe <kyawswe411@gmail.com>
2024-04-15 12:13:13 +0630

    sss

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -33,6 +33,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'gender' => $input['gender'],
             'password' => Hash::make($input['password']),
             'phone' =>$input['phone'],
             'address'=>$input['address'],
